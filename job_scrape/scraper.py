88e805e44dfac3e43dcfece9dde8aa913f95508a
Pongpun364 <pongpun364@hotmail.com>
2021-11-22 23:15:07 +0700

    get link data 1 pages

---
+++ job_scrape/scraper.py
@@ -0,0 +1,47 @@
+import asyncio
+from arsenic import get_session,browsers, services
+import pathlib
+
+
+BASE_DIR = pathlib.Path().parent
+# print('BASE_DIR == ' ,BASE_DIR)
+EXE_PATH = str(BASE_DIR / "driver" / "chromedriver.exe")
+
+
+async def perform_scrape(session, url, delay):
+    await session.get(url)
+    
+    curr_heigth = await session.execute_script("return document.body.scrollHeight")
+    # print('curr_heght ==',curr_heigth)
+    i = 0
+    while True:
+        await session.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        await asyncio.sleep(delay)
+        i += 1
+        # print(f'{i} time of sleep')
+        iter_height = await session.execute_script("return document.body.scrollHeight")
+        # print('iter heght ==', iter_height)
+        if iter_height == curr_heigth:
+            # print('i am out man !')
+            break
+        curr_heigth = iter_height
+    
+    return await session.get_page_source()
+
+
+
+
+async def scraper(url, i=-1, timeout = 120, start=None, delay=15):
+    service = services.Chromedriver(binary=EXE_PATH)
+    browser = browsers.Chrome()
+    browser.capabilities = {
+        "goog:chromeOptions": {"args": ["--headless", "--disable-gpu"]}
+    }
+    async with get_session(service, browser) as session:
+        try:
+            body = await asyncio.wait_for(perform_scrape(session, url, delay), timeout = timeout)
+        except asyncio.TimeoutError:
+            print('time out fuckkkk!!!')
+            return []   
+
+        return body
---
+++ job_scrape/website/indeed.py
@@ -0,0 +1,143 @@
+import random
+import asyncio
+
+import pandas as pd
+from requests_html import HTML
+import itertools
+import re
+import time
+import pathlib
+import logging
+import structlog # pip install structlog
+from urllib.parse import urlparse
+# from ..storage import list_to_sql, df_from_sql, df_to_sql
+from ..logging import  set_arsenic_log_level
+from ..scraper import scraper
+from ..storage import df_to_csv
+
+
+async def extract_id(url_path):
+    regex = r"([0-9a-z]+)\Sfccid"
+    my_match = re.findall(regex, url_path)
+    if not my_match:
+        return None
+    return my_match[0]
+
+async def extract_salary(result):
+    data = result.find('.salary-snippet', first=True)
+    if data != None:
+        regex = r"[\d,]+"
+        my_match = re.findall(regex, data.text)
+        if not my_match:
+            return None, None
+        return my_match[0].replace(',', '') , my_match[1].replace(',', '')
+    return None, None
+
+
+async def get_link_data(body):
+    datas = []
+    html_r = HTML(html=body)
+    for result in html_r.find('.result'):
+        try:
+            title = result.find('.jobTitle', first=True).text
+        except:
+            title = None
+        try:
+            path = result.attrs['href']
+            id_ = extract_id(path) 
+        except:
+            id_ = None
+        
+        min_salary , max_salary = extract_salary(result)
+        data = {
+            'id' : id_,
+            'title': title,
+            'min_salary': min_salary,
+            'max_salary': max_salary
+        }
+        datas.append(data)
+    return datas   
+
+
+
+
+
+
+async def indeed_scraper(url, i=-1, timeout = 120, start=None, delay=15):
+
+    body = await scraper(url,i=i, timeout = timeout, start=start, delay=delay)
+
+    links = await get_link_data(body)
+    # product_data = await get_product_data(url, body )
+    
+    
+    if start != None:
+        end = time.time() - start
+        print(f'{i} took {end} seconds')
+    
+    dataset = {
+        'links': links,
+        'job_desc': None
+    }
+    return dataset
+
+
+
+async def run(urls, start=None):
+    results = []
+    for i, url in enumerate(urls):
+        results.append(
+            asyncio.create_task(indeed_scraper(url, i=i, timeout=120, start=start ,delay = 10))
+        )
+    list_of_links = await asyncio.gather(*results)
+    return list_of_links
+
+
+# def get_saved_urls(limit=5):
+
+    # links_df = df_from_sql('spoonflower_links')
+    # urls = []
+    # scraped_ids = []
+    # used_df = False
+
+    # if not links_df.empty:
+    #     sub_link_df = links_df.copy()
+    #     sub_link_df = sub_link_df[sub_link_df['scraped'] == 0]
+    #     sub_link_df = sub_link_df.sample(limit)
+    #     urls = [f'https://www.spoonflower.com{x}'for x in sub_link_df['path'].to_list()]
+    #     scraped_ids = sub_link_df.id.to_list()
+    #     if len(urls) > 0:
+    #         used_df = True
+    # return urls, scraped_ids, used_df
+
+
+
+# def get_list_range(limit=10, is_random=True, random_max=150):
+#     urls = []
+#     for i in range(limit):
+#         if is_random:
+#             page = random.randint(i+1, random_max)
+#         else:
+#             page = i + 1
+#         urls.append(f"https://www.spoonflower.com/en/shop?on=fabric&page_offset={page}")
+#     return urls
+
+
+
+def run_indeed():
+    set_arsenic_log_level()
+    start = time.time()
+    urls = ['https://th.indeed.com/jobs?q=python+developer&l=กรุงเทพ&start=0' ]
+
+    results = asyncio.run(run(urls, start = start))
+    end = time.time() - start
+    print('total time =', end)
+    links = [x['links'] for x in results ] # [[],[],[]]
+    links = itertools.chain.from_iterable(links)
+    links = list(links)
+    link_columns = ['id', 'title', 'min_salary', 'max_salary', 'scraped']
+    df_to_csv(columns=link_columns)
+
+
+if __name__ == "__main__":
+    run_indeed()
